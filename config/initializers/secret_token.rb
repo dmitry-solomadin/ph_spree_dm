13fc2620b50b9866e5dae0696875dd2872990d87
Dmitry Solomadin <dmitry.solomadin@gmail.com>
2013-02-05 13:54:41 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ph::Application.config.secret_token = '2c1e3f5d6b1c742f7f895b2d8dce78158be46716b1e6cc817d6d3aa6db0310db9cfd23cf4938a1d259f818144467ee8146f8b0320f7bf6fc05fde4f3f25f48c7'
