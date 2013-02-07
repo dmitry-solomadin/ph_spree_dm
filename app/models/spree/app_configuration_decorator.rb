a9be5d4d1d5cf40d9b22a3a1cd4224003496b729
Dmitry Solomadin <dmitry.solomadin@gmail.com>
2013-02-07 15:55:05 +0200

    fox

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,4 +1,4 @@
 Spree::AppConfiguration.class_eval do
   preference :ph_store_credit_minimum , :float, :default => 30.0
 end
-Spree::Config.use_store_credit_minimum = 0
+Spree::Config.use_store_credit_minimum = 0 # do not change, this is done to disable spree_store's validation.
